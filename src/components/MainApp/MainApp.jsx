a0ab8812e07af16b4cccf27b6e6b8ef4b8a948ec
Sujil Anto <sujilnt@gmail.com>
2018-06-02 20:30:57 +0100

    A small refactor

--- src/components/MainApp/MainApp.jsx
+++ src/components/MainApp/MainApp.jsx
@@ -1,6 +1,5 @@
 import React ,{Component} from 'react' ;
 import Player from "../PlayerComponent/PlayerComponent.js";
-import InputRangeSlider from "../inputRangeSlider/inputRangeSlider.js";
 import "../commonCssStyles/browserReset.scss";
 import './MainApp.scss';
 
@@ -9,7 +8,6 @@ class MainApp extends Component {
         return(
             <div className="mainContainer">
                 <Player/>
-                <InputRangeSlider/>
             </div>
         );
     }
