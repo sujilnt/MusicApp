e0af6bbbe09a3818c359804388a7ae7ddb85a78c
Sujil Anto <sujilnt@gmail.com>
2018-06-22 23:07:27 +0100

    Updates

--- postcss.config.js
+++
@@ -1,9 +0,0 @@
-const autoprefixer = require('autoprefixer');
-
-module.exports = {
-    plugins: [
-        //   'postcss-import': {},
-        autoprefixer({ browsers: ['last 2 version', 'ie 9'] })
-        //   'cssnano': {}
-    ]
-}
